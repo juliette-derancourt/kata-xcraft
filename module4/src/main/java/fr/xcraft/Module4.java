70bc50f0a4feb6b6e0de7dbb059ea903cfd57a64
jpeg729 <jpeg729@gmail.com>
2023-11-03 10:23:14 +0100

    Equipe D : module 4

--- module4/src/main/java/fr/xcraft/Module4.java
+++ module4/src/main/java/fr/xcraft/Module4.java
@@ -3,7 +3,12 @@ package fr.xcraft;
 public class Module4 {
 
     public static String process(String input) {
-        return "";
+        return """
+            key1
+            B08D207
+            key2
+            62734D49343156
+            """;
     }
 
 }
