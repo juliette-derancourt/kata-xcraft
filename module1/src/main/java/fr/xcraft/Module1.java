8f101104511f009b3f9ae26dbdcd76af5bd2d68a
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2023-11-03 10:22:44 +0100

    Module 1 get the  3 message in sercret1 .md in back quote

--- module1/src/main/java/fr/xcraft/Module1.java
+++ module1/src/main/java/fr/xcraft/Module1.java
@@ -1,9 +1,22 @@
 package fr.xcraft;
 
+import java.io.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Module1 {
 
     public static String process(String input) {
-        return "";
+        Pattern pattern= Pattern.compile("```(.*)```");
+        Matcher matcher=pattern.matcher(input);
+        if (matcher.find()){
+            String output=matcher.group(1);
+            System.out.println(output);
+        }else{
+            System.out.println("pas de code");
+        }
+
+        return input;
     }
 
 }
