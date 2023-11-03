f88e340661340013e3e7199efa0bd38ede00009d
jpeg729 <jpeg729@gmail.com>
2023-11-03 11:14:14 +0100

    Equipe D : module 2

--- module2/src/main/java/fr/xcraft/Module2.java
+++ module2/src/main/java/fr/xcraft/Module2.java
@@ -1,15 +1,27 @@
 package fr.xcraft;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class Module2 {
 
     public static String process(String input) {
         String output = "";
-        String[] listOfMorseLetter = input.split(" ");
+        String[] listOfMorseLetter = extractMorseFromInput(input);
         for (int i = 0; i < listOfMorseLetter.length; i++) {
 
             output += fromMorse(listOfMorseLetter[i]);
         }
-        return output;
+        return Base64.getEncoder().encodeToString(output.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static String[] extractMorseFromInput(String input) {
+        // le input va contenir les données pour le module2 puis un séparateur
+        // puis les données pour le module3 puis un séparateur
+        // puis les données pour le module4
+        String[] lines = input.stripLeading().split("\n\n");
+        // apparament c'est que la deuxième ligne du morse qu'il faut traduire
+        return lines[0].split("\n")[1].split(" ");
     }
 
     private static String fromMorse(String s) {
