a91e8055353a42a8435493aa67ff366f85656189
benoit-glevarec <glevarec.benoit@gmail.com>
2023-11-03 10:20:24 +0100

    sorry

--- module2/src/main/java/fr/xcraft/Module2.java
+++ module2/src/main/java/fr/xcraft/Module2.java
@@ -3,7 +3,10 @@ package fr.xcraft;
 public class Module2 {
 
     public static String process(String input) {
-        return "KBsKASEWZ3AMFG4XWCRSXT0eBnVXdWx0AXsjV2IdGhtaBiZjZWkVUnpoVGUXWhxwVGQdIXQBHm1+E3VoExA=";
+        if(input == "") {
+            return "KBsKASEWZ3AMFG4XWCRSXT0eBnVXdWx0AXsjV2IdGhtaBiZjZWkVUnpoVGUXWhxwVGQdIXQBHm1+E3VoExA=";
+        }
+        return "";
     }
 
 }
