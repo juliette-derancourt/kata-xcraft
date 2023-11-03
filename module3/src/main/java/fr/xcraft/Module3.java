6e79fa7296f57d5c6af2cd34f5f099868e7e4bba
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2023-11-03 10:57:07 +0100

    Module 3 parse key message and translate

--- module3/src/main/java/fr/xcraft/Module3.java
+++ module3/src/main/java/fr/xcraft/Module3.java
@@ -6,26 +6,28 @@ public class Module3 {
 
     public static String process(String input) {
         return processLine(input);
-        /*
-         * 
-         String[] lines = input.split("\n");
- 
-         String[] splitInput = input.split(" \\[x");
-         String key = splitInput[0];
-         String times = splitInput[1];
- 
-         return times;
-         */
+
     }
 
     public static String processLine(String input) {
-        String[] splitInput = input.split(" \\[x");
-        String key = splitInput[0];
-        if(splitInput.length == 1) {
+        String splitInput = """
+key1 [x2]
+key2
+key1 [x1]
+""";
+        String[] lignes = splitInput.split("\n");
+
+        // Parcourir et afficher chaque ligne
+        for (String ligne : lignes) {
+            // Trier par ordre ascendant
+            System.out.println(ligne);
+        }
+        String key = lignes[0];
+        if(lignes.length == 1) {
             return key;
         }
         
-        String timesString = splitInput[1].substring(0, splitInput[1].length()-1);
+        String timesString = lignes[1].substring(0, lignes[1].length()-1);
         Integer times = Integer.parseInt(timesString);
         
         return "";
